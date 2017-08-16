1e97b1491a6516a06b83c8e919c3f5d09e2a523c
Nejc Ribic <nejc.ribic95@gmail.com>
2017-08-16 21:39:34 +0200

    page completed fixing navigation bar

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function dropdownMenu() {
         x.className = "topnav";
     }
 }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
